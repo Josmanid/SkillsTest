03634dddf693a551d84ff50a33062478b155ceda
Jonas Martin Smidt <jos005@edu.zealand.dk>
2024-06-14 09:45:36 +0200

    Opgave 2

---
+++ SkillsTest/Booking.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsTest
+{
+    internal class Booking
+    {
+
+        public int ID { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int Participants { get; set; }
+
+
+
+        public Booking(int id, DateTime start, DateTime end, int participants)
+        {
+            ID = id;
+            Start = start;
+            End = end;
+            Participants = participants;
+        }
+
+
+        public override string ToString() {
+            string message = "Your booking Id is: " + ID + " You booking starts at: " + Start+" And ends at: "+End+" with the following number of participants: "+Participants;
+            return message;
+        }
+
+
+    }
+}
--- SkillsTest/Program.cs
+++ SkillsTest/Program.cs
@@ -3,7 +3,34 @@
     internal class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Opgave 2.");
+
+            // Test klassen ved at oprette nogle instanser af den i C# filen program.cs og skriv dem ud til 
+            //konsollen med Console.WriteLine
+            DateTime start1 = new DateTime(2024, 6, 15, 12, 30, 0);
+            DateTime end1 = new DateTime(2024, 6, 15, 14, 30, 0);
+            Booking bk1 = new Booking(1, start1, end1, 12);
+
+            Console.WriteLine(bk1);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Opgave 3.");
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
